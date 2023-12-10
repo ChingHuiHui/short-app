b6dc68dba3e614c0ee91102e617ebc75529854db
chinghuihui <sunny70624@gmail.com>
2023-12-10 18:08:19 +0800

    remove persist on store

--- src/stores/useVideosStore.ts
+++ src/stores/useVideosStore.ts
@@ -1,5 +1,5 @@
 import { create } from 'zustand'
-import { createJSONStorage, devtools, persist } from 'zustand/middleware'
+import { devtools } from 'zustand/middleware'
 import type { Short } from '../type'
 
 interface VideosState {
@@ -11,32 +11,22 @@ const CHANNEL_ID = 'UC9zY_E8mcAo_Oq772LEZq8Q'
 const URL = `https://yt.lemnoslife.com/channels?part=shorts&id=${CHANNEL_ID}`
 
 export const useVideosStore = create<VideosState>()(
-  devtools(
-    persist(
-      (set) => ({
-        videos: [],
-        fetch: async () => {
-          const data = await fetch(URL)
-          const res = await data.json()
+  devtools((set) => ({
+    videos: [],
+    fetch: async () => {
+      const data = await fetch(URL)
+      const res = await data.json()
 
-          const videos = (res?.items[0]?.shorts as Short[]).map(
-            (short: Short) => ({
-              ...short,
-              currentTime: 0,
-            })
-          )
+      const videos = (res?.items[0]?.shorts as Short[]).map((short: Short) => ({
+        ...short,
+        currentTime: 0,
+      }))
 
-          set({
-            videos,
-          })
+      set({
+        videos,
+      })
 
-          return videos
-        },
-      }),
-      {
-        name: 'videos-storage',
-        storage: createJSONStorage(() => localStorage),
-      }
-    )
-  )
+      return videos
+    },
+  }))
 )
