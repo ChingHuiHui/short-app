58591aee2a349cc6d007ae8c15be1591aaef53e6
chinghuihui <sunny70624@gmail.com>
2023-12-10 17:41:02 +0800

    sort out App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
-import {useState, Suspense } from 'react'
+import { useState, Suspense } from 'react'
 import './App.css'
 import VideoList from './components/VideoList'
 
-
 export default function App() {
   const [start, setStart] = useState(false)
+  
   return (
     <div>
       {
-        !start &&
-        <div className="fixed inset-0 bg-black z-50 flex justify-center items-center">
-          <button className='p-1 text-3xl hover:bg-white hover:text-black' onClick={() => setStart(true)}> VIEW THE SHORTS (CLICK ME)</button>
-        </div>
+        start ?
+          <Suspense fallback={<div className='flex justify-center items-center h-[100dvh]'>Loading...</div>}>
+            <VideoList />
+          </Suspense>
+        : (
+          <div className="fixed inset-0 bg-black z-50 flex justify-center items-center">
+            <button className='p-1 text-3xl hover:bg-white hover:text-black' onClick={() => setStart(true)}> VIEW THE SHORTS (CLICK ME)</button>
+          </div>
+        )
       }
-      <Suspense fallback={<div className='flex justify-center items-center h-[100dvh]'>Loading...</div>}>
-        <VideoList />
-      </Suspense>
     </div>
   )
 }
