69d8c5c5d38467cdc5c4769a1d1b40eaa9e87a20
chinghuihui <sunny70624@gmail.com>
2023-12-10 18:07:12 +0800

    create ErrorBoundary

--- src/main.tsx
+++ src/main.tsx
@@ -6,13 +6,34 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import { ErrorBoundary } from 'react-error-boundary';
+
+const Fallback = ({ error, resetErrorBoundary }: { error: Error, resetErrorBoundary: () => void }) => {
+  return (
+    <div className='flex flex-col justify-center items-center h-[100dvh]'>
+      <div className='max-w-[768px] space-y-6'>
+        <h2 className='text-3xl'>Something went wrong:</h2>
+        <div className='border p-2 w-full'>
+          <pre className='text-red-500'>{error.message}</pre>
+        </div>
+        <button className='underline font-bold' onClick={resetErrorBoundary}>Try Again</button>
+      </div>
+    </div>
+  );
+}
+
 
 const router = createBrowserRouter([
   {
     path: "/",
     element:  (
       <React.StrictMode>
-        <App />
+        <ErrorBoundary 
+          fallbackRender={Fallback}
+          onReset={() => window.location.reload()}
+        >
+          <App />
+        </ErrorBoundary>
       </React.StrictMode>
     )
   }
