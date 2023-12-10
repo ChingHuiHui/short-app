131893886f22ab1e409439d82b0454148a2680db
chinghuihui <sunny70624@gmail.com>
2023-12-10 14:44:23 +0800

    add type Short

---
+++ src/type.d.ts
@@ -0,0 +1,9 @@
+export type Short = {
+  videoId: string
+  title: string
+  thumbnails: { url: string; width: string; height: string }[]
+  string: string
+  channelHandle: string
+  channelId: string
+  currentTime?: number
+}
