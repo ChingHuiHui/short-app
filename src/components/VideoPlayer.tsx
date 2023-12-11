f9fa6aeecc2f101a1943451fafbf96fc5012343d
chinghuihui <sunny70624@gmail.com>
2023-12-11 13:25:45 +0800

    add replay function on video && sort out video

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,5 +1,5 @@
 import ReactPlayer from 'react-player'
-import { useEffect, useState, memo, useMemo } from "react"
+import { useEffect, useState, memo, useMemo, useRef } from "react"
 
 import PlayIcon from '../assets/play.svg?react'
 import PauseIcon from '../assets/pause.svg?react'
@@ -7,7 +7,6 @@ import PauseIcon from '../assets/pause.svg?react'
 import VolumeOn from '../assets/volume-on.svg?react'
 import VolumeOff from '../assets/volume-off.svg?react'
 import VideoActions from "./VideoActions"
-
 
 enum VOLUME {
   ON = 0.5,
@@ -23,9 +22,11 @@ const VideoPlayer = memo(
   ({ id, thumbnail, isActive, title }: { id: string, title: string, thumbnail: string, isActive: boolean }) => {  
     const [state, setState] = useState(STATE.PAUSED)
     const [volume, setVolume] = useState(VOLUME.OFF)
-
+    
     const [currentTime, setCurrentTime] = useState(0)
-    const [showControls, setShowControls] = useState(false)
+    const [isReady, setIsReady] = useState(false)
+    
+    const player = useRef<ReactPlayer>(null)
 
     useEffect(() => {      
       if(!isActive) {
@@ -47,18 +48,28 @@ const VideoPlayer = memo(
 
     function ready() {
       console.log('video is ready')
-      setShowControls(true)
+      setIsReady(true)
+    }
+
+    function track({ playedSeconds }: {playedSeconds: number}) {
+      setCurrentTime(Math.floor(playedSeconds))
+    }
+
+    function replay() {
+      setCurrentTime(0)
+      player.current?.seekTo(0)
     }
     
     return (
       <div className="flex items-end mb-10 space-x-4">
         <div className='group'>
           <div className="aspect-[4/7] w-[20rem] relative rounded-lg overflow-hidden">
-            <div  className='absolute inset-0' style={{backgroundImage: `url(${thumbnail})`, display: isActive ? 'none': 'block'}}></div>
+            <div  className='absolute inset-0' style={{backgroundImage: `url(${thumbnail})`, display: isActive && isReady ? 'none': 'block'}}></div>
             <div className='absolute inset-0'>
               {
                 isActive && (
                   <ReactPlayer  
+                  ref={player}
                     playing={isPlaying}
                     volume={volume}
                     config={{
@@ -67,22 +78,20 @@ const VideoPlayer = memo(
                       },
                     }}
                     controls={false}
+                    loop={false}
                     width='100%'
                     height='100%'
-                    loop={true}
                     url={`https://www.youtube.com/watch?v=${id}`}
                     onReady={ready} 
                     onPlay={() => setState(STATE.PLAYING)}
                     onPause={() => setState(STATE.PAUSED)}
-                    onProgress={({ playedSeconds }) => {
-                      setCurrentTime(Math.floor(playedSeconds))
-                    }} 
-                    onEnded={() => setCurrentTime(0)}
+                    onProgress={track} 
+                    onEnded={replay}
                   />
                 )
               }
             </div>
-            {showControls && <nav className="opacity-0 group-hover:opacity-100 p-4 pt-8 absolute bottom-0 w-full text-white bg-gradient-to-t from-black/70 to-transparent rounded-lg">
+            {isReady && <nav className="opacity-0 group-hover:opacity-100 p-4 pt-8 absolute bottom-0 w-full text-white bg-gradient-to-t from-black/70 to-transparent rounded-lg">
               <ul className="flex justify-between">
                 <li>
                   <button className="w-6 h-6" onClick={toggleState}>
